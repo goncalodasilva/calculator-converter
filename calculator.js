3f81e54f0daa332b7a284f1bdc7ea403a790a67b
goncalodasilva <gnunosilva@hotmail.com>
2023-03-27 17:50:40 +0200

    first commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const display = document.getElementById('display');
+
+// Attach event listeners to each button
+const buttons = document.querySelectorAll('td');
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Do the logic here
+  });
+});
